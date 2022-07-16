54662109675f9dc0c158272a3f8471440e43bc18
Aymeric Ingargiola <aymericfbm@gmail.com>
2022-07-16 17:16:23 +0200

    add audio player

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHashHistory } from 'vue-router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faUserSecret,
+  faDownload,
+  faFastBackward,
+  faFastForward,
+  faStopCircle,
+  faPauseCircle,
+  faPlayCircle,
+  faListAlt,
+  faVolumeUp,
+  faVolumeDown,
+  faVolumeOff,
+  faVolumeMute,
+  faRandom,
+  faChevronRight,
+  faChevronLeft,
+  faGlobeEurope,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import Home from '@/pages/HomePage.vue';
 
 const router = createRouter({
@@ -13,9 +33,30 @@ const router = createRouter({
   ],
 });
 
+library.add(
+  faUserSecret,
+  faDownload,
+  faFastBackward,
+  faFastForward,
+  faStopCircle,
+  faPauseCircle,
+  faPlayCircle,
+  faListAlt,
+  faVolumeUp,
+  faVolumeDown,
+  faVolumeOff,
+  faVolumeMute,
+  faRandom,
+  faChevronRight,
+  faChevronLeft,
+  faGlobeEurope,
+);
+
 const app = createApp(Home);
 
 app.use(router);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.config.globalProperties.$isDev = process.env.NODE_ENV === 'development';
 
